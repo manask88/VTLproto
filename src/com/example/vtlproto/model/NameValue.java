f974c239da3d158dc364f0a7439016c35a5e942c
Manuel <manask88@gmail.com>
2013-04-03 22:34:05 -0400

    added last files

---
+++ src/com/example/vtlproto/model/NameValue.java
@@ -0,0 +1,28 @@
+package com.example.vtlproto.model;
+
+public class NameValue {
+
+	String name;
+	boolean booleanValue;
+	char charValue;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean getBooleanValue() {
+		return booleanValue;
+	}
+	public void setValue(boolean booleanValue) {
+		this.booleanValue = booleanValue;
+	}
+	
+	public char getCharValue() {
+		return charValue;
+	}
+	public void setValue(char charValue) {
+		this.charValue = charValue;
+	}
+	
+}
